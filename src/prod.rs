3d6167318b9a93cb3cf9e42db263825cf3cba36f
Chris Copeland <chris@chrisnc.net>
2022-04-05 02:52:22 -0700

    simplify term interfaces and use Box in prod

--- src/prod.rs
+++ src/prod.rs
@@ -4,28 +4,28 @@ use crate::{Operator, Term};
 
 // TODO: allow subterms to have lifetimes shorter than the parent
 #[derive(Clone, Debug)]
-pub enum Prod<'a> {
+pub enum Prod {
     One,
     Mul {
-        left: &'a Term<String, Prod<'a>>,
-        right: &'a Term<String, Prod<'a>>,
+        left: Box<Term<String, Prod>>,
+        right: Box<Term<String, Prod>>,
     },
 }
 use Prod::*;
 
 pub enum Iter<'a> {
     Empty,
-    Right(&'a Term<String, Prod<'a>>),
+    Right(&'a Term<String, Prod>),
     Both {
-        left: &'a Term<String, Prod<'a>>,
-        right: &'a Term<String, Prod<'a>>,
+        left: &'a Term<String, Prod>,
+        right: &'a Term<String, Prod>,
     },
 }
 
 impl<'a> Iterator for Iter<'a> {
-    type Item = &'a Term<String, Prod<'a>>;
+    type Item = &'a Term<String, Prod>;
 
-    fn next(&mut self) -> Option<&'a Term<String, Prod<'a>>> {
+    fn next(&mut self) -> Option<&'a Term<String, Prod>> {
         match *self {
             Iter::Empty => None,
             Iter::Right(right) => {
@@ -40,23 +40,23 @@ impl<'a> Iterator for Iter<'a> {
     }
 }
 
-impl<'a> Prod<'a> {
+impl Prod {
     pub fn mul(
-        left: &'a Term<String, Prod<'a>>,
-        right: &'a Term<String, Prod<'a>>,
-    ) -> Term<String, Prod<'a>> {
-        Term::Op(Mul { left, right })
+        left: Term<String, Prod>,
+        right: Term<String, Prod>,
+    ) -> Term<String, Prod> {
+        Term::Op(Mul { left: Box::new(left), right: Box::new(right) })
     }
 
-    pub fn var<S: Into<String>>(s: S) -> Term<String, Prod<'a>> {
+    pub fn var<S: Into<String>>(s: S) -> Term<String, Prod> {
         Term::Var(s.into())
     }
 
-    pub fn one() -> Term<String, Prod<'a>> {
+    pub fn one() -> Term<String, Prod> {
         Term::Op(One)
     }
 
-    pub fn iter(&'a self) -> Iter {
+    pub fn iter(&self) -> Iter<'_> {
         match self {
             One => Iter::Empty,
             Mul { left, right } => Iter::Both { left, right },
@@ -64,11 +64,11 @@ impl<'a> Prod<'a> {
     }
 }
 
-impl<'a> Operator<'a> for Prod<'a> {
+impl Operator for Prod {
     type Var = String;
-    type ArgIter = Iter<'a>;
+    type ArgIter<'a> = Iter<'a>;
 
-    fn arg_iter(&'a self) -> Self::ArgIter {
+    fn arg_iter(&self) -> Iter {
         match self {
             One => Iter::Empty,
             Mul { left, right } => Iter::Both { left, right },
@@ -85,7 +85,7 @@ impl<'a> Operator<'a> for Prod<'a> {
 }
 
 fn fmt_with_parens<'a, V: fmt::Display>(
-    t: &Term<V, Prod<'a>>,
+    t: &Term<V, Prod>,
     f: &mut fmt::Formatter,
 ) -> fmt::Result {
     match t {
@@ -94,7 +94,7 @@ fn fmt_with_parens<'a, V: fmt::Display>(
     }
 }
 
-impl<'a> Prod<'a> {
+impl Prod {
     fn fmt_with_parens(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             One => "1".fmt(f),
@@ -103,7 +103,7 @@ impl<'a> Prod<'a> {
     }
 }
 
-impl<'a> fmt::Display for Prod<'a> {
+impl fmt::Display for Prod {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
         match self {
             One => write!(f, "1"),
@@ -122,8 +122,8 @@ mod tests {
     fn display() {
         let a = Prod::var("a");
         let b = Prod::var("b");
-        let m = Prod::mul(&a, &b);
-        let am = Prod::mul(&a, &m);
+        let m = Prod::mul(a.clone(), b.clone());
+        let am = Prod::mul(a.clone(), m.clone());
         println!("{}", m);
         println!("{}", am);
     }
@@ -134,12 +134,12 @@ mod tests {
         let b = Prod::var("b");
         let c = Prod::var("c");
         let d = Prod::var("d");
-        let tab = Prod::mul(&a, &b);
-        let tcd = Prod::mul(&c, &d);
-        let taa = Prod::mul(&a, &a);
-        let tbb = Prod::mul(&b, &b);
-        assert!(tab.eq(&tcd));
-        assert!(taa.eq(&tbb));
-        assert!(!taa.eq(&tab));
+        let tab = Prod::mul(a.clone(), b.clone());
+        let tcd = Prod::mul(c.clone(), d.clone());
+        let taa = Prod::mul(a.clone(), a.clone());
+        let tbb = Prod::mul(b.clone(), b.clone());
+        assert_eq!(tab, tcd);
+        assert_eq!(taa, tbb);
+        assert!(taa != tab);
     }
 }
--- src/term.rs
+++ src/term.rs
@@ -9,26 +9,24 @@ pub enum Term<V, O> {
 }
 pub use Term::*;
 
-// TODO: allow arguments to have lifetimes shorter than the parent term
-pub trait Operator<'a>: Sized {
+pub trait Operator: Sized {
     type Var: Ord;
-    type ArgIter: Iterator<Item = &'a Term<Self::Var, Self>>
+    type ArgIter<'a>: Iterator<Item = &'a Term<Self::Var, Self>>
     where
         Self: 'a;
 
-    fn arg_iter(&'a self) -> Self::ArgIter;
-
+    fn arg_iter(&self) -> Self::ArgIter<'_>;
     fn opeq(&self, other: &Self) -> bool;
 }
 
-impl<'a, V: Ord, O: Operator<'a, Var = V>> Term<V, O> {
+impl<V: Ord, O: Operator<Var = V>> Term<V, O> {
     /// Do an inorder traversal and collect a list of numbers representing the variables. Each
     /// unique variable is mapped to a different number, starting from 1 and increasing in the
     /// order of each variable first appears in the traversal.
-    pub fn varseq(&'a self) -> Vec<u64> {
+    pub fn varseq(&self) -> Vec<u64> {
         let mut n: u64 = 0;
         let mut varmap: BTreeMap<&V, u64> = BTreeMap::new();
-        let mut terms: VecDeque<&'a Self> = VecDeque::new();
+        let mut terms: VecDeque<&Self> = VecDeque::new();
         let mut vars: Vec<u64> = vec![];
         terms.push_back(self);
         while let Some(t) = terms.pop_front() {
@@ -51,24 +49,32 @@ impl<'a, V: Ord, O: Operator<'a, Var = V>> Term<V, O> {
     }
 
     /// Determine if two terms have the same operator structure, ignoring variables.
-    pub fn varopeq<'b: 'a>(&'a self, other: &'b Term<V, O>) -> bool {
+    pub fn varopeq(&self, other: &Term<V, O>) -> bool {
         match (self, other) {
             (Var(_), Var(_)) => true,
-            (Op(f), Op(g)) => f.opeq(g) && f.arg_iter().zip(g.arg_iter()).all(|(ft, gt)| ft.varopeq(gt)),
+            (Op(f), Op(g)) => {
+                f.opeq(g)
+                    && f.arg_iter()
+                        .zip(g.arg_iter())
+                        .all(|(ft, gt)| ft.varopeq(gt))
+            }
             _ => false,
         }
     }
 
+}
+
+impl<V: Ord, O: Operator<Var = V>> PartialEq for Term<V, O> {
     // TODO: figure out how to implement the eq from PartialEq. For now, the lifetimes don't match.
     /// Two terms are equal if they have the same operator structure and the variable sequence.
-    pub fn eq<'b: 'a>(&'a self, other: &'b Term<V, O>) -> bool {
+    fn eq(&self, other: &Term<V, O>) -> bool {
         self.varopeq(other) && self.varseq() == other.varseq()
     }
 }
 
 /*
-impl<'a, 'l: 'a, 'r: 'a, V: Ord, O: Operator<'a, Var = V>> PartialEq for Term<V, O> {
-    fn eq(&'l self, other: &'r Term<V, O>) -> bool {
+impl<V: Ord, O: Operator<'_, Var = V>> PartialEq for Term<V, O> {
+    fn eq(&self, other: &Term<V, O>) -> bool {
         self.varopeq(other) && self.varseq() == other.varseq()
     }
 }
